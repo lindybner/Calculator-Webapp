745874fb16b12b3ef4afed80c3160f304b0abd9e
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-01-27 15:33:41 -0500

    compound interest calculator markup/section added

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,19 @@
 function showCalculator() {
   document.getElementById("calculator").classList.remove("hidden");
   document.getElementById("tally").classList.add("hidden");
+  document.getElementById("compound-interest").classList.add("hidden");
 }
 
 function showTallyCounter() {
   document.getElementById("tally").classList.remove("hidden");
   document.getElementById("calculator").classList.add("hidden");
+  document.getElementById("compound-interest").classList.add("hidden");
+}
+
+function showCompoundInterestCalculator() {
+  document.getElementById("compound-interest").classList.remove("hidden");
+  document.getElementById("calculator").classList.add("hidden");
+  document.getElementById("tally").classList.add("hidden");
 }
 
 // Show calculator by default
@@ -20,3 +28,6 @@ document
 document
   .getElementById("tally-link")
   .addEventListener("click", showTallyCounter);
+document
+  .getElementById("compound-interest-link")
+  .addEventListener("click", showCompoundInterestCalculator);
