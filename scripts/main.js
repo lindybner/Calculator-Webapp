898002417c31bab663ad47e8cee9394001831937
Jim Lin-Dybner <lin.jycl@gmail.com>
2024-01-28 12:38:09 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,28 @@ function showCalculator() {
   document.getElementById("calculator").classList.remove("hidden");
   document.getElementById("tally").classList.add("hidden");
   document.getElementById("compound-interest").classList.add("hidden");
+  document.getElementById("investment-calculator").classList.add("hidden"); // Hide investment calculator
 }
 
 function showTallyCounter() {
   document.getElementById("tally").classList.remove("hidden");
   document.getElementById("calculator").classList.add("hidden");
   document.getElementById("compound-interest").classList.add("hidden");
+  document.getElementById("investment-calculator").classList.add("hidden"); // Hide investment calculator
 }
 
 function showCompoundInterestCalculator() {
   document.getElementById("compound-interest").classList.remove("hidden");
   document.getElementById("calculator").classList.add("hidden");
   document.getElementById("tally").classList.add("hidden");
+  document.getElementById("investment-calculator").classList.add("hidden"); // Hide investment calculator
+}
+
+function showInvestmentCalculator() {
+  document.getElementById("investment-calculator").classList.remove("hidden");
+  document.getElementById("calculator").classList.add("hidden");
+  document.getElementById("tally").classList.add("hidden");
+  document.getElementById("compound-interest").classList.add("hidden");
 }
 
 // Show calculator by default
@@ -31,3 +41,6 @@ document
 document
   .getElementById("compound-interest-link")
   .addEventListener("click", showCompoundInterestCalculator);
+document
+  .getElementById("investment-calculator-link")
+  .addEventListener("click", showInvestmentCalculator);
